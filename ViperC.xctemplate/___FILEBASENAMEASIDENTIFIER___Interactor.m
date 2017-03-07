0ae29e78bdc8b74cf738926c6b60799692a0e62a
abdullahselek <abdullahselek@gmail.com>
2017-03-07 19:01:26 +0100

    - add interactor

---
+++ ViperC.xctemplate/___FILEBASENAMEASIDENTIFIER___Interactor.m
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+ 
+#import "___FILEBASENAMEASIDENTIFIER___Interactor.h"
+ 
+@implementation ___FILEBASENAMEASIDENTIFIER___Interactor
+ 
+@end
