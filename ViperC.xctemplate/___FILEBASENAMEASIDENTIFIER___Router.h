b51370ab455b64a9a4ec0dc7243a36cf6cf8c443
abdullahselek <abdullahselek@gmail.com>
2017-03-07 19:06:31 +0100

    - add router

---
+++ ViperC.xctemplate/___FILEBASENAMEASIDENTIFIER___Router.h
@@ -0,0 +1,14 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "___FILEBASENAMEASIDENTIFIER___InteractorProtocol.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___Router: NSObject<___FILEBASENAMEASIDENTIFIER___WireframeProtocol>
+
+@end
---
+++ ViperC.xctemplate/___FILEBASENAMEASIDENTIFIER___Router.m
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+ 
+#import "___FILEBASENAMEASIDENTIFIER___Router.h"
+ 
+@implementation ___FILEBASENAMEASIDENTIFIER___Router
+ 
+@end
