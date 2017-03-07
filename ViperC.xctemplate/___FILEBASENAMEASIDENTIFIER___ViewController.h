4d1756b9a2268f57e0f997201302cd6057c631db
abdullahselek <abdullahselek@gmail.com>
2017-03-07 19:27:13 +0100

    - add template viewcontroller

---
+++ ViperC.xctemplate/___FILEBASENAMEASIDENTIFIER___ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "___FILEBASENAMEASIDENTIFIER___InteractorProtocol.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ViewController: UIViewController<___FILEBASENAMEASIDENTIFIER___ViewProtocol>
+
+@property (nonatomic, nullable) id<___FILEBASENAMEASIDENTIFIER___PresenterProtocol> presenter;
+
+@end
---
+++ ViperC.xctemplate/___FILEBASENAMEASIDENTIFIER___ViewController.m
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___FILEBASENAMEASIDENTIFIER___ViewController.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+} 
+
+@end
