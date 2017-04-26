d40f285cc9df841ea54e04fcb24db52316136186
abdullahselek <abdullahselek@gmail.com>
2017-04-26 22:25:44 +0200

    create tableview cell

---
+++ Sample/ToDoTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ToDoTableViewCell.h
+//  Sample
+//
+//  Created by Abdullah Selek on 26/04/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToDoTableViewCell : UITableViewCell
+
+@end
---
+++ Sample/ToDoTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.m
+//  Sample
+//
+//  Created by Abdullah Selek on 26/04/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import "ToDoTableViewCell.h"
+
+@implementation ToDoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
