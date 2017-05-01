a6d325f629cedef73d80e1f539237b0e57ef1200
abdullahselek <abdullahselek@gmail.com>
2017-05-01 11:26:23 +0200

    create tests unit for interactor add swift version updater

---
+++ SampleTests/ToDoInteractorTests.m
@@ -0,0 +1,18 @@
+//
+//  ToDoInteractorTests.m
+//  Sample
+//
+//  Created by Abdullah Selek on 01/05/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <Quick/Quick.h>
+#import "ToDoInteractor.h"
+
+@interface ToDoInteractor (Tests)
+
+@end
+
+QuickSpecBegin(ToDoInteractorTests)
+
+QuickSpecEnd
