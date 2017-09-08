d674e7ba9e49810590949dcfcccf15e1cdd6c59a
Abdullah Selek <abdullahselek@gmail.com>
2017-09-08 17:32:37 +0200

    better indentation for presenter

--- ViperC-Quick.xctemplate/Swift/___FILEBASENAMEASIDENTIFIER___InteractorTests.swift
+++ ViperC-Quick.xctemplate/Swift/___FILEBASENAMEASIDENTIFIER___InteractorTests.swift
@@ -21,7 +21,9 @@ class ___FILEBASENAMEASIDENTIFIER___InteractorTests: QuickSpec {
         beforeSuite {
             let router = ___FILEBASENAMEASIDENTIFIER___Router()
             view = ___FILEBASENAMEASIDENTIFIER___Router.createModule() as? ___FILEBASENAMEASIDENTIFIER___ViewController
-            presenter = ___FILEBASENAMEASIDENTIFIER___Presenter(interface: view, interactor: interactor, router: router)
+            presenter = ___FILEBASENAMEASIDENTIFIER___Presenter(interface: view,
+                                                                interactor: interactor,
+                                                                router: router)
 
             view.presenter = presenter
             interactor.presenter = presenter
