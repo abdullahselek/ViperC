790e3674de78c5c1e5d60bf9cef5173cf1e798cf
Abdullah Selek <abdullahselek@gmail.com>
2018-01-21 11:37:11 +0000

    XCTest template for ViewController with Objective-C

---
+++ ViperC-XCTest.xctemplate/Objective-C/___FILEBASENAMEASIDENTIFIER___ViewControllerTests.m
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "___VARIABLE_viperModuleName___ViewController.h"
+#import "___VARIABLE_viperModuleName___Router.h"
+
+@interface ___VARIABLE_viperModuleName___ViewControllerTests : XCTestCase
+
+@end
+
+@implementation ___VARIABLE_viperModuleName___ViewControllerTests
+
+- (void)testCreateModule
+{
+    ___VARIABLE_viperModuleName___ViewController *viewController = (___VARIABLE_viperModuleName___ViewController *) [___VARIABLE_viperModuleName___Router createModule];
+    XCTAssertNotNil(viewController);
+}
+
+@end
