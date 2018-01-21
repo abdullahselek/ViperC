0b0060caa127156ed24d370965b159d341cb5545
Abdullah Selek <abdullahselek@gmail.com>
2018-01-21 17:55:07 +0000

    XCTest template for Router with Swift

---
+++ ViperC-XCTest.xctemplate/Swift/___FILEBASENAMEASIDENTIFIER___RouterTests.swift
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import XCTest
+
+//@testable import YOUR_MODULE_NAME
+
+class ___VARIABLE_viperModuleName___RouterTests: XCTestCase {
+
+    func testCreateModule() {
+        let view = ___VARIABLE_viperModuleName___Router.createModule() as? ___VARIABLE_viperModuleName___ViewController
+        XCTAssertNotNil(view)
+    }
+
+}
